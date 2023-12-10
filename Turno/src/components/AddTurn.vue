b1644bac2569bac8c05ac64a2952b9fd0a1c0bfe
Andressalasf <humkyjimp@gmail.com>
2023-12-10 18:28:46 -0500

    ejercicio de turnos

---
+++ Turno/src/components/AddTurn.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const modulo1 = ref([{codigo: '', index:0}])
+const modulo2 = ref([{codigo: '', index:0}])
+const modulo3 = ref([{codigo: '', index:0}])
+
+
+</script>
+
+<template>
+   
+   <div class="container flex W-1/2 mx-auto my-0 ">
+
+    <h1>SISTEMAS DE TURNOS</h1>
+
+        <div>
+            <div>
+
+            </div>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+   </div>
+  
+</template>
+
+<style scoped></style>
